ab1f8264bc82b0d9426327cc5379136cd1cde429
Salih AKSU <salihaksu66@gmail.com>
2024-03-17 02:40:20 +0300

    First Calculator App

---
+++ HesapMakinesi/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  HesapMakinesi
+//
+//  Created by Salih on 16.03.2024.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
